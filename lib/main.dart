4513497aee1dfd7526c961b758a4460738e7819b
Sanjarbek Olimov <Sanjarbek071@gmail.com>
2022-01-18 15:14:35 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:login_ui/pages/home_page.dart';
+import 'package:login_ui/pages/login_page.dart';
+import 'package:login_ui/pages/sign_up_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SignUpPage(),
+      routes: {
+        HomePage.id: (context) => HomePage(),
+        LoginPage.id: (context) => LoginPage(),
+        SignUpPage.id: (context) => SignUpPage()
+      },
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginPage extends StatefulWidget {
+  static const String id = "login_page";
+
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  void fireToast(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+        fontSize: 16);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                Colors.green.shade900,
+                Colors.green,
+                Colors.green.shade400,
+              ])),
+          child: Column(
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: const [
+                        // #login_text
+                        Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white, fontSize: 32.5),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+
+                        // #welcome_back
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  )),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                          topRight: Radius.circular(50))),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 60,
+                      ),
+
+                      // #text_field
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 30),
+                        height: 120,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.shade300,
+                                  blurRadius: 20,
+                                  spreadRadius: 10,
+                                  offset: Offset(0, 10))
+                            ]),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            TextField(
+                              decoration: InputDecoration(
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 10),
+                                  border: InputBorder.none,
+                                  hintText: "Email",
+                                  hintStyle: TextStyle(color: Colors.grey)),
+                            ),
+                            Divider(
+                              thickness: 0.5,
+                              height: 10,
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 10),
+                                  border: InputBorder.none,
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(color: Colors.grey)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 35,
+                      ),
+
+                      // #login_button
+                      MaterialButton(
+                        onPressed: () {},
+                        height: 45,
+                        minWidth: 240,
+                        shape: const StadiumBorder(),
+                        color: Colors.green.shade700,
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+
+                      // #text
+                      const Text(
+                        "Login with SNS",
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+
+                      // #buttons(facebook & github)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          MaterialButton(
+                            onPressed: () {},
+                            color: Colors.blue,
+                            shape: StadiumBorder(),
+                            height: 45,
+                            minWidth: 150,
+                            child: Text(
+                              "Facebook",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          MaterialButton(
+                            onPressed: () {},
+                            color: Colors.black,
+                            shape: const StadiumBorder(),
+                            height: 45,
+                            minWidth: 150,
+                            child: const Text(
+                              "Github",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                      
+                      Expanded(child: Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        alignment: Alignment.bottomCenter,
+                        child: RichText(
+                          text: TextSpan(
+                            text: "Powered by ",
+                            style: TextStyle(color: Colors.black, fontSize: 14),
+                            children: [
+                              TextSpan(
+                                text: "#PDP ",
+                                style: TextStyle(color: Colors.blue),
+                                recognizer: TapGestureRecognizer()..onTap = (){
+                                  fireToast("Hash tag One");
+                                }
+                              ),
+                              TextSpan(
+                                text: "#Academy ",
+                                style: TextStyle(color: Colors.blue),
+                                recognizer: TapGestureRecognizer()..onTap = (){
+                                  fireToast('Hash tag Two');
+                                }
+                              ),
+                            ]
+                          ),
+                        ),
+                      ))
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
